404b46961562d46d85444f56fb3fe61453153220
lukesummers5 <luke.summers@outlook.com>
2024-09-25 11:10:08 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -58,15 +58,10 @@ module "blog_alb" {
   target_groups = [
     {
       name_prefix = "blog"
-      backend_protocol = "HTTP"
-      backend_port = 80
+      protocol = "HTTP"
+      port = 80
       target_type = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port = 80
-        }
-      }
+      target_id = aws_instance.blog.id
     }
   ]
 
