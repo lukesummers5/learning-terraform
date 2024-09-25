68c0b9d824c55bd0ea9f6271a48bc1b793d07dfc
lukesummers5 <luke.summers@outlook.com>
2024-09-25 11:38:04 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -65,7 +65,7 @@ module "blog_alb" {
       protocol = "HTTP"
       port = 80
       target_type = "instance"
-      target_id = module.autoscaling
+      target_id = module.autoscaling.autoscaling_group_id
     }
   }
 
